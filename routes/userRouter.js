75ccbee64a93b6799ef818da25ed380f5d48e8aa
Poorna Sachinthana <poornasachinthana2241@gmail.com>
2026-03-23 20:01:29 +0530

    update user router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,16 +1,28 @@
 import express from 'express';
-import {createUser, deleteUser, updateUser, loginUser } from '../controllers/userController.js';
+import {createUser, deleteUser, updateUser, loginUser,googleLogin,sendOTP,validateOTPAndUpdatePassword,getUser, getAllUsers,updateUserStatus } from '../controllers/userController.js';
 
 
 
 const userRouter = express.Router(); // Create a router(village) for user
 
+userRouter.post("/",createUser);
+
 userRouter.post("/login",loginUser);
 
-userRouter.post("/",createUser);
+userRouter.get("/", getUser);
 
 userRouter.delete("/",deleteUser);      
 
 userRouter.put("/", updateUser);
+
+userRouter.post("/google-login", googleLogin);
+
+userRouter.get("/send-otp/:email", sendOTP);
+
+userRouter.post("/validate-otp", validateOTPAndUpdatePassword);
+
+userRouter.get("/all",getAllUsers)
+
+userRouter.put("/toggle-block/:email", updateUserStatus)
 
 export default userRouter;
